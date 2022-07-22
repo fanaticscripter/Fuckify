d4b19961553ee0c10848d105e3e03d27cc38923c
Zhiming Wang <i@zhimingwang.org>
2022-07-22 15:22:50 +0800

    Log source code link to console

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import App from './App.vue';
 import FontFaceObserver from 'fontfaceobserver';
 import { fonts } from '@/fonts';
 
+console.log(
+  '%cSource code: https://github.com/fanaticscripter/Fuckify',
+  'color: #f97316; font-size: 20px'
+);
+
 (async () => {
   for (const font of fonts) {
     await new FontFaceObserver(font).load();
